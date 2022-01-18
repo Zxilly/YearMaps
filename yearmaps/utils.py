f54e032f8555d15e66e4a14dfb3c9d58576ccd2e
Zxilly <zhouxinyu1001@gmail.com>
2022-01-18 16:26:48 +0800

    feat: enhance utils

--- yearmaps/utils.py
+++ yearmaps/utils.py
@@ -5,28 +5,25 @@ class ProviderError(Exception):
     pass
 
 
-def ensure_default_data_dir() -> Path:
+def default_data_dir() -> Path:
     home = Path.home()
     if not home.exists():
         raise ProviderError("No home directory found")
     data_dir = home / ".yearmaps"
-    if not data_dir.exists():
-        data_dir.mkdir()
-        return data_dir
+    return ensure_dir(data_dir)
+
+
+def cache_dir(data_dir: Path = default_data_dir()) -> Path:
+    cache_dir = data_dir / "cache"
+    return ensure_dir(cache_dir)
+
+
+def ensure_dir(dir_: Path) -> Path:
+    if not dir_.exists():
+        dir_.mkdir()
+        return dir_
     else:
-        if not data_dir.is_dir():
+        if not dir_.is_dir():
             raise ProviderError("Default data directory is not a directory")
         else:
-            return data_dir
-
-
-def ensure_cache_dir(data_dir: Path = ensure_default_data_dir()) -> Path:
-    cache_dir = data_dir / "cache"
-    if not cache_dir.exists():
-        cache_dir.mkdir()
-        return cache_dir
-    else:
-        if not cache_dir.is_dir():
-            raise ProviderError("Cache directory is not a directory")
-        else:
-            return cache_dir
+            return dir_
