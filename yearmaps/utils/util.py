2d67b59af0939574e087589e27cd1b0dab913703
Zxilly <zhouxinyu1001@gmail.com>
2022-03-27 20:10:47 +0800

    style: make pylint happy
    Signed-off-by: Zxilly <zhouxinyu1001@gmail.com>

--- yearmaps/utils/util.py
+++ yearmaps/utils/util.py
@@ -24,7 +24,7 @@ def option_name(name: str) -> str:
 
 def update_time(cache_dir: str):
     path = Path(cache_dir) / ".update_time"
-    with open(path, 'w') as f:
+    with open(path, 'w', encoding="UTF-8") as f:
         f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
 
@@ -32,5 +32,5 @@ def get_update_time(cache_dir: str) -> str:
     path = Path(cache_dir) / ".update_time"
     if not path.exists():
         return "Never updated."
-    with open(path, 'r') as f:
+    with open(path, 'r', encoding="UTF-8") as f:
         return str(f.read())
